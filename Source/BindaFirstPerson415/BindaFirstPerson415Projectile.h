07f9ebe82d1d75c77edc1effa7527f21c317d03b
clbwhazzup <camlbinda@gmail.com>
2025-01-21 13:13:36 -0500

    Created cube whose material parameters change on overlap, and projectile component gets a random mesh and splatter color and random splatter texture

--- Source/BindaFirstPerson415/BindaFirstPerson415Projectile.h
+++ Source/BindaFirstPerson415/BindaFirstPerson415Projectile.h
@@ -14,17 +14,42 @@ class ABindaFirstPerson415Projectile : public AActor
 {
 	GENERATED_BODY()
 
-	/** Sphere collision component */
+	// Sphere collision component
 	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
-	USphereComponent* CollisionComp;
+		USphereComponent* CollisionComp;
 
-	/** Projectile movement component */
+	// Projectile movement component
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	UProjectileMovementComponent* ProjectileMovement;
+		UProjectileMovementComponent* ProjectileMovement;
+
+	// Projectile mesh component
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* ballMesh;
+	
+	// Matrerial for mesh
+	UPROPERTY(EditAnywhere)
+		UMaterial* baseMat;
+
+	// Color for material
+	UPROPERTY(EditAnywhere)
+		FLinearColor randColor;
+
+	// Dynamic material actually used for mesh
+	UPROPERTY(EditAnywhere)
+		UMaterialInstanceDynamic* dmiMat;
+
+	// Material interface used for creating dynamic material
+	UPROPERTY(EditAnywhere)
+		UMaterialInterface* projMat;
+
 
 public:
 	ABindaFirstPerson415Projectile();
 
+protected:
+	void BeginPlay();
+
+public:
 	/** called when projectile hits something */
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
---
+++ Source/BindaFirstPerson415/CubeDMIMod.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "CubeDMIMod.generated.h"
+
+UCLASS()
+class BINDAFIRSTPERSON415_API ACubeDMIMod : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACubeDMIMod();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// For overlap
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* boxComp;
+
+	// Cube mesh component
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* cubeMesh;
+
+	// Material interface used for creating dynamic material
+	UPROPERTY(EditAnywhere)
+		UMaterialInterface* baseMat;
+
+	// Dynamic material instance so parameters can be changed
+	UPROPERTY()
+		UMaterialInstanceDynamic* dmiMat;
+
+	// Function for overlap, used to change material parameters
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
